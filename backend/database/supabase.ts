aa5f52fd27942a3063a331f48c15a1e31fee3c4d
Zsófia Samodai <100917399+zsofiasamodai@users.noreply.github.com>
2025-06-01 13:58:52 +0200

    Updated supabase.ts

--- backend/database/supabase.ts
+++ backend/database/supabase.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
+if (!process.env.https://mgytbuyekpyiktjaelnt.supabase.co || !process.env.SUPABASE_ANON_KEY) {
   throw new Error('Missing Supabase environment variables');
 }
 
